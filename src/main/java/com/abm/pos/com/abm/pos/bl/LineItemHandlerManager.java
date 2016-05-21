8badfe52d303368d6a59e3c9136af1e53712eb80
Alok Patel <alok_s_patel1@homedepot.com>
2016-05-21 13:12:12 -0400

    refectored code

---
+++ src/main/java/com/abm/pos/com/abm/pos/bl/LineItemHandlerManager.java
@@ -0,0 +1,7 @@
+package com.abm.pos.com.abm.pos.bl;
+
+/**
+ * Created by asp5045 on 5/18/16.
+ */
+public class LineItemHandlerManager {
+}
---
+++ src/main/java/com/abm/pos/com/abm/pos/dto/AddTransactionDto.java
@@ -0,0 +1,7 @@
+package com.abm.pos.com.abm.pos.dto;
+
+/**
+ * Created by asp5045 on 5/20/16.
+ */
+public class AddTransactionDto {
+}
